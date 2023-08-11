3c4631fd1d183aafc128c674bea5b8c9189e3681
Баянбилэг Баттөгс <HP233137@M301-CC0139.local>
2023-08-11 15:36:18 +0800

    hello world

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/api/hello',
+  cache: new InMemoryCache(),
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <ApolloProvider client={client}>
+   <Component {...pageProps} />
+  </ApolloProvider>)
 }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,90 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import db from "./utils/db";
 
-type Data = {
-  name: string
-}
+const typeDefs = `#graphql
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  type Post {
+    text: String
+    image:[String]
+    userId: String
+    createdAt: String
+  }
+
+  type Query {
+    getPosts: [Post],
+    getPostDetail(code: String): Post
+
+  }
+
+  input PostInput {
+    text: String
+    image:String    
+  }
+
+  type Mutation {
+    createPost(PostCreateInput: PostInput!): Post,
+    updatePost(id: ID!, PostUpdateInput: PostInput!): Post,
+    deletePost(id: ID!): Boolean
+  }
+
+`;
+
+const resolvers = {
+  Query: {
+    getPosts: async () => {
+      const GetPosts = await db("find", {});
+      return GetPosts.documents;
+    },
+
+    // getPostDetail: (_, args) => {
+
+    //   const GetPost = await db("findOne", {});
+
+    //   return GetPost.document;
+
+    // },
+  },
+
+  Mutation: {
+    createPost: async (_: any, args: any) => {
+      const CreatePost = await db("insertOne", {
+        document: { 
+          text: args.PostCreateInput.text,
+          image: args.PostCreateInput.image,
+        },
+      });
+
+      return CreatePost;
+    },
+    deletePost: async (_: any, args: any) => {
+      const deletePost = await db("deleteOne", {
+        filter: {
+          _id: { $oid: args.id },
+        },
+      });
+
+      return deletePost;
+    },
+
+    updatePost: async (_: any, args: any) => {
+      const updatePost = await db("updateOne", {
+        filter: {
+          _id: { $oid: args.id },
+        },
+        update: {
+          $set: {
+            text: args.PostUpdateInput.text,
+          },
+        },
+      });
+      return updatePost;
+    },
+  },
+};
+
+const Server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+export default startServerAndCreateNextHandler(Server);
